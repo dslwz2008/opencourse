68d6bdabc0c831394b23f677292d64c324e31d5b
dslwz2008 <dslwz2008@gmail.com>
2014-09-18 13:33:14 +0800

    dsa add tutorial codes

---
+++ dsa-thu/dsa-assignments/tutorials/add.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char * argv[]){
+	int a = 0, b = 0;
+	cin >> a >> b;
+	cout << (a+b) << endl;
+
+	//int c;
+	//cin >> c;
+	return 0;
+}
