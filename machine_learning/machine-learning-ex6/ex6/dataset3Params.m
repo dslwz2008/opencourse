80dbfa38a4d4ec685ea98b4a748acacf42293c10
dslwz2008 <dslwz2008@gmail.com>
2015-09-29 16:53:32 +0800

    finish ex6

--- machine_learning/machine-learning-ex6/ex6/dataset3Params.m
+++ machine_learning/machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,22 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-
-
-
-
-
+vals=[0.01,0.03,0.1,0.3,1,3,10,30];
+minerr = 1e9;
+for i=1:size(vals,2)
+    for j=1:size(vals,2)
+        tempC = vals(i);
+        tempSigma = vals(j);
+        model= svmTrain(X, y, tempC, @(x1, x2) gaussianKernel(x1, x2, tempSigma)); 
+        predictions = svmPredict(model, Xval);
+        err = mean(double(predictions ~= yval));
+        if err < minerr
+            minerr = err;
+            C = tempC;
+            sigma = tempSigma;
+        end
+    end
+end
 
 % =========================================================================
 
