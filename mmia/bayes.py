2b4160d034c0c99a0f6b045164f26a6c7079c868
dslwz2008 <dslwz2008@gmail.com>
2015-12-01 18:28:24 +0800

    add bayes.py

---
+++ mmia/bayes.py
@@ -0,0 +1,42 @@
+# -*-coding:utf-8-*-
+# Author: Shen Shen
+# Email: dslwz2002@163.com
+__author__ = 'Shen Shen'
+
+import numpy as np
+
+
+def loadDataSet():
+    postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                   ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                   ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                   ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                   ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                   ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0, 1, 0, 1, 0, 1]
+    return postingList, classVec
+
+
+def createVocabList(dataSet):
+    vocabSet = set([])
+    for document in dataSet:
+        vocabSet = vocabSet | set(document)# 两个集合的并集
+    return list(vocabSet)
+
+
+def setOfWords2Vec(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else:
+            print("the word: %s is not in my Vocabulary!" % word)
+    return returnVec
+
+
+if __name__ == '__main__':
+    listOPosts, listClasses = loadDataSet()
+    myVocabList = createVocabList(listOPosts)
+    print(myVocabList)
+    print(setOfWords2Vec(myVocabList,listOPosts[0]))
+    print(setOfWords2Vec(myVocabList,listOPosts[3]))
