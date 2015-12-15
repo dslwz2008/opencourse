2017298982060512f04e300f37cd73c1110716a6
dslwz2008 <dslwz2008@gmail.com>
2015-12-15 15:39:09 +0800

    rename files

---
+++ mlia/kNN.py
@@ -0,0 +1,149 @@
+# -*-coding:utf-8-*-
+# Author: Shen Shen
+# Email: dslwz2002@163.com
+__author__ = 'Shen Shen'
+
+import numpy as np
+import operator
+
+
+def createDataSet():
+    group = np.array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances ** 0.5
+    sortedDistIndices = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndices[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortedClassCount = sorted(classCount.iteritems(),
+                              key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMat = np.zeros((numberOfLines, 3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOLines:
+        listFromLine = line.strip().split('\t')
+        returnMat[index, :] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = np.zeros(np.shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - np.tile(minVals, (m, 1))
+    normDataSet = normDataSet / np.tile(ranges, (m, 1))
+    return normDataSet, ranges, minVals
+
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :],
+                                     datingLabels[numTestVecs:m], 3)
+        print('the classifier came back with: %d the real answer is : %d'
+              % (classifierResult, datingLabels[i]))
+        if classifierResult != datingLabels[i]:
+            errorCount += 1.0
+    print('the total error rate is: %f' % (errorCount / numTestVecs))
+
+
+def classifyPerson():
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    percentTats = float(raw_input('percentage of time spent playing video games?'))
+    ffMiles = float(raw_input('frequent flier miles earned per year?'))
+    iceCream = float(raw_input('liters of the ice cream consumed per year?'))
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr = np.array([ffMiles, percentTats, iceCream])
+    classifierResult = classify0((inArr - minVals) / ranges, normMat, datingLabels, 3)
+    print('You will probably like this person:', resultList[classifierResult - 1])
+
+
+def img2vector(filename):
+    returnVect = np.zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(lineStr[j])
+    return returnVect
+
+
+def handwritingClassTest():
+    from os import listdir
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')
+    m=len(trainingFileList)
+    trainingMat = np.zeros((m,1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i, :] = img2vector('trainingDigits/%s' % fileNameStr)
+
+    testFileList = listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print('the classifier came back with: %d, the real answer is: %d'
+              % (classifierResult, classNumStr))
+        if classifierResult != classNumStr:
+            errorCount += 1.0
+    print('\nthe total number of errors is: %d' % errorCount)
+    print('\nthe total error rate is: %f' % (errorCount/mTest))
+
+if __name__ == '__main__':
+    # g,l = createDataSet()
+    # print(classify0([0,0], g, l, 3))
+
+    import matplotlib.pyplot as plt
+
+    # datingDataMat, datingLabels=file2matrix('datingTestSet2.txt')
+    # print(datingDataMat)
+    # print(datingLabels)
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    # # ax.scatter(datingDataMat[:,1], datingDataMat[:,2])
+    # ax.scatter(datingDataMat[:,0], datingDataMat[:,1], 15.0*np.array(datingLabels),
+    #            15.0*np.array(datingLabels))
+    # plt.show()
+
+    # normMat, ranges, minVals = autoNorm(datingDataMat)
+    # print(normMat)
+    # print(ranges)
+    # print(minVals)
+
+    # datingClassTest()
+    # classifyPerson()
+    handwritingClassTest()
