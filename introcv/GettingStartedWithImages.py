8102632eca75a9ed89370f04bce1766f1513e0e7
dslwz2008 <dslwz2008@gmail.com>
2016-01-06 17:30:20 +0800

    add week 6

--- introcv/GettingStartedWithImages.py
+++ introcv/GettingStartedWithImages.py
@@ -4,6 +4,24 @@
 import numpy as np
 import cv2
 
+def main():
+    img = cv2.imread('data/messi5.jpg',cv2.IMREAD_UNCHANGED)
+    noise = img.copy()
+    cv2.randn(noise,30,10)
+    imgNoised = img + noise
+
+    cv2.namedWindow('raw', cv2.WINDOW_NORMAL)
+    cv2.imshow('raw', img)
+    cv2.namedWindow('test', cv2.WINDOW_NORMAL)
+    cv2.imshow('test', imgNoised)
+
+    k = cv2.waitKey(0)
+    if k == 27:         # wait for ESC key to exit
+        cv2.destroyAllWindows()
+    elif k == ord('s'): # wait for 's' key to save and exit
+        cv2.imwrite('test.png',img)
+        cv2.destroyAllWindows()
+
 
 def main():
     img = cv2.imread('data/messi5.jpg',cv2.IMREAD_UNCHANGED)
